ef20cae758fd7c0fc3eb684ddb6d5fc9af926982
karolo <karoloarchive@gmail.com>
2025-06-04 00:22:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,48 @@
 let slideIndex = 1;
-showSlides(slideIndex);
+let slideInterval;
+
+document.addEventListener('DOMContentLoaded', function() {
+  showSlides(slideIndex);
+  startAutoPlay();
+});
 
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+  resetAutoPlay();
+  showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+  resetAutoPlay();
+  showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("slides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+function startAutoPlay() {
+  slideInterval = setInterval(() => {
+    plusSlides(1);
+  }, 5000); 
+}
+
+function resetAutoPlay() {
+  clearInterval(slideInterval);
+  startAutoPlay();
 }
