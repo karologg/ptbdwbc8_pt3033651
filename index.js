fdc750b2a26d95a9641cd3598244ecc071b401fd
karolo <karoloarchive@gmail.com>
2025-06-04 00:20:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ function showSlides(n) {
     if (n > slides.length) {
         slideIndex = 1;
     }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
